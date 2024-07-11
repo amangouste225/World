49a84454e6a2504718561da13cbf7c6de6fa14ef
Amangoua Kacou Ange-Thierry <104840429+amangouste225@users.noreply.github.com>
2024-07-11 22:58:51 +0000

    react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,18 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Pricing from "./pages/Pricing.tsx";
+import Homepage from "./pages/Homepage.tsx";
+import Product from "./pages/Product.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
